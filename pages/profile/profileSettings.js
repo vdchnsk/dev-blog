919593d402570ba358aa0199969a3db6a60fa96c
Valeriy Dichanskiy <dichansky@gmail.com>
2021-04-22 14:52:15 +0300

    created profile settings UI

--- pages/profile/profileSettings.js
+++ pages/profile/profileSettings.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import { MainLayout } from '../../components/MainLayout'
 import styles from '../../styles/profileSettings.module.scss'
+import { Button, TextField } from '@material-ui/core'
 
 export default function profileSettings ({userId}) {
     const router = useRouter()
@@ -20,7 +21,13 @@ export default function profileSettings ({userId}) {
                 <div className={styles.profileSettings_img}></div>
                 <div className={styles.profileSettings}>
                     <h1>Настройки профиля</h1>
+                    <TextField className={styles.profileSettings__input} color={"secondary"} id="standard-required" label="nickname" defaultValue="" />
+                    <TextField className={styles.profileSettings__input} color={"secondary"} id="standard-required" label="email" defaultValue="" />
+                    <TextField className={styles.profileSettings__input} color={"secondary"} id="standard-required" label="password" defaultValue="" />
+                    {/* <TextField color={"secondary"} id="standard-required" label="repeat password" defaultValue="" /> */}
+                    <Button className={styles.profileSettings__button} color={"secondary"} variant="contained">Save</Button>
                 </div>
+                
                 
             </div>
         </MainLayout>
