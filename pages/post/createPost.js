bfebc42ca5c8a5b73dab1f2aae4335699ae18898
Valeriy Dichanskiy <dichansky@gmail.com>
2021-05-10 17:42:50 +0300

    🚧 Added tag adding functional

--- pages/post/createPost.js
+++ pages/post/createPost.js
@@ -2,23 +2,62 @@ import { useEffect, useRef, useState } from "react";
 import { TextareaAutosize, TextField } from "@material-ui/core";
 import { MainLayout } from "../../components/MainLayout";
 import PublishIcon from '@material-ui/icons/Publish';
+import {PostTags} from "../../components/posts/PostTags"
+var randomColor = require('randomcolor');
 
 
 
-export default function CreatePost({}){
+export default function CreatePost({tags}){
     const [postTitle, setPostTitle] = useState("")
     const [postDescription, setPostDescription] = useState("")
     const [postBody, setPostBody] = useState("")
-    const [postPreview, setPostPreview] = useState("Articel Body")
+    const [postPreview, setPostPreview] = useState("")
+
+    const [apiTags, setApiTags] = useState({tags})
+    const [isAutoCompleteOpen, setIsAutoCompleteOpen] = useState(true)
+    const [postTagsFilled, setPostTagsFilled] = useState("")
     const [postTags, setPostTags] = useState([])
 
     const modifyText =  tag => {
         setPostBody(postBody + tag)
         bodyInput.current.focus()
     }
-    useEffect(() => {
 
-    }, [postPreview])
+    const filtredTags = tags.filter(tag=>{
+        return tag.value.toLowerCase().includes(postTagsFilled.toLowerCase())
+    })
+    const ACItemClickHandler = (e) =>{
+        setPostTagsFilled(e.target.textContent)
+        setIsAutoCompleteOpen(false)
+        addToPostTags({tags}, e.target.textContent)
+        
+    }
+    const setIsAutoCompleteOpenHandler = ()=>{
+        setIsAutoCompleteOpen(true)
+    }
+    const addToPostTags = ({tags}, exectTag)=>{
+        if(tags.findIndex(i => i.value === exectTag)){
+            let indexOfElement = tags.findIndex(i => i.value === exectTag)
+            postTags.push(tags[indexOfElement])
+            setPostTagsFilled("")
+
+        }else{
+            let newTag = {
+                "id":Math.floor(Math.random() * 1000000) * 1,
+                "value":exectTag,
+                "color":randomColor()
+            }
+            postTags.push(newTag)
+            setPostTagsFilled("")
+        }
+        return 
+    }
+    const clearTags = ()=>{
+        setPostTags([])
+    }
+
+    // console.log(tags)
+    // console.log(tags.findIndex(i=>i.value === "Microsoft"))
     const bodyInput = useRef()
     return (
         <MainLayout title={"Create new post"}>
@@ -35,17 +74,21 @@ export default function CreatePost({}){
                                 aria-label="maximum height" 
                                 placeholder="Description of your article"/>
                             <label htmlFor="upload" style={{cursor:"pointer", width:"24%",margin:"10px 0px"}} >
-                                 <div style={{padding:"5px", borderRadius:"3px", display:"flex",justifyContent:"center",alignItems:"center", background:"black", color:"white", position:"relative"}}>
+                                 <div style={{padding:"5px", borderRadius:"3px", display:"flex",justifyContent:"center",alignItems:"center", background:"black", color:"white"}}>
                                     <input onChange={(e) => setPostPreview(e.target.value)} id="upload" style={{cursor:"pointer", height:"100%", width:"100%", opacity:"0",zIndex:"10", position:"absolute"}} type="file" hidden/>
                                     <PublishIcon/>
-                                    <span style={{marginLeft:"1px",whiteSpace:"nowrap", overflow:"hidden", textOverflow:"ellipsis"} }>{postPreview}</span>
-                                </div>
+                                    <span style={{marginLeft:"1px",whiteSpace:"nowrap", overflow:"hidden", textOverflow:"ellipsis"} }>{postPreview !== ""?postPreview:"Post preview"}</span>
+                                </div> 
                             </label>
                         </div>
                         <div className="newPost__content__main__aricleInput">
                             <div className="newPost__content__main__aricleInput__toolbar">
                                 <button onClick={()=>{modifyText("<strong></strong>")}} className="toolbar__element"><strong>bold</strong></button>
                                 <button onClick={()=>{modifyText("<i></i>")}} className="toolbar__element"><i>italic</i></button>
+                                <button onClick={()=>{modifyText("<h1></h1>")}} className="toolbar__element"><span>h1</span></button>
+                                <button onClick={()=>{modifyText("<h2></h2>")}} className="toolbar__element"><span>h2</span></button>
+                                <button onClick={()=>{modifyText("<h3></h3>")}} className="toolbar__element"><span>h3</span></button>
+                                <button onClick={()=>{modifyText("<h4></h4>")}} className="toolbar__element"><span>h4</span></button>
                                 <button onClick={()=>{modifyText("•")}} className="toolbar__element">list</button>
                                 <button onClick={()=>{modifyText("<img></img>")}} className="toolbar__element" style={{borderRight:"2px solid black"}}>image</button>
                             </div>
@@ -56,13 +99,24 @@ export default function CreatePost({}){
                                 style={{overflow:"auto", minHeight:"400px", minWidth:"100%", maxWidth:"100%", padding:"10px", height:"150px", width:"50%", fontFamily:"Roboto"}} 
                                 rowsMax={10} 
                                 aria-label="maximum height" 
-                                placeholder="Write down your comment!"/>
+                                placeholder="Body of your article"/>
                         </div>
                     </div>
                     <div className="newPost__content__footer">
-                        <div className="footer__tags">
-                            <span>Choose not more than 3 suiteble tags</span>
-                            <TextField style={{width:"60%",margin:"10px 0 25px 0px"}} className="metaInput" color={"secondary"} label="suiteble tag" />
+                        <div className="footer__tags " style={{margin:"0px 0 25px 0px", display:"flex", flexDirection:"row"}}>
+                            <div className="footer__tags__item footer__tags__tagChoosing" style={{display:"flex", flexDirection:"column"}}>
+                                <span style={{width:"100%", fontWeight:"500"}}>Choose not more than 3 suiteble tags</span>
+                                <TextField onClick={setIsAutoCompleteOpenHandler} onChange={(e) => setPostTagsFilled(e.target.value)} value={postTagsFilled} style={{width:"53%", position:"relative"}} className="metaInput" color={"secondary"} label="suiteble tag" />
+                                <ul style={{listStyle:"none", padding:"0", margin:"0", width:"53%"}} className="tag__autocomplete">
+                                    {
+                                        postTagsFilled && isAutoCompleteOpen ? filtredTags.map((tag) => { return<li onClick={ACItemClickHandler} className="tag__autocomplete__item" key={tag.id}>{tag.value}</li>}) : null
+                                    }
+                                </ul>
+                            </div>
+                            <div className="footer__tags__item footer__tags__suitebleTag">
+                                <span style={{width:"100%", fontWeight:"500"}}>Tags: <span >{postTags.length !== 0 ?<span onClick={clearTags} className="clearTags" >clear</span>:null}</span></span>
+                                <PostTags tagsList={postTags}/>
+                            </div>
                         </div>
                         <button className="footer__tags__button">Next</button>
                     </div>
@@ -121,6 +175,29 @@ export default function CreatePost({}){
                     background:black;
                     color:white;
                 }
+                .footer__tags__item{
+                    width:50%;
+                }
+                .tag__autocomplete__item{
+                    padding:10px 10px 10px 0px;
+                    cursor:pointer;
+                    background:white;
+                    border-bottom:1px solid gainsboro;
+                }
+                .tag__autocomplete__item:hover, .tag__autocomplete__item:focus{
+                    background:whitesmoke;
+                    transition:.3s;
+                }
+                .clearTags{
+                    font-weight: 400;
+                    opacity: 70%;
+                    cursor:pointer;
+                }
+                .clearTags:hover, .clearTags:focus{
+                    transition:.3s;
+                    text-decoration:underline;
+                } 
+
                 input[type=file]::-webkit-file-upload-button {
                     display:"none";
                 }
@@ -143,4 +220,16 @@ export default function CreatePost({}){
             </style>
         </MainLayout>
     )
+}
+export async function getServerSideProps({req}) {
+    if (!req){
+        return {tags : null}
+    }
+    const responce = await fetch(`http://localhost:4200/tags`)
+    const tags = await responce.json()
+
+    return {
+        props: {tags:tags}
+    }
+        
 }
