def8d628da8c2946628a8f2ffca42284a59c8b5d
Valeriy Dichanskiy <dichansky@gmail.com>
2021-05-30 23:28:52 +0300

    added reCaptcha

--- pages/auth/client/LogInPage.js
+++ pages/auth/client/LogInPage.js
@@ -3,13 +3,14 @@ import Button from '@material-ui/core/Button';
 import { MainLayout } from "../../../components/MainLayout";
 import styles from '../../../styles/auth.module.scss'
 import Link from "next/link"
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useAuth } from "../../hooks/auth.hook";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/router";
 import { useHttp } from "../../hooks/useHttp";
 import { showAlert } from "../../redux/actions/alertActions";
 import { Notification } from "../../../components/Notification";
+import ReCAPTCHA from "react-google-recaptcha"
 
 export default function LogInPage () {
     const [nickanmeOrLogin , setNickanmeOrLogin] = useState("")
@@ -18,19 +19,24 @@ export default function LogInPage () {
     const {login}  = useAuth()
     const dispatch = useDispatch()
     const router = useRouter()
+    const reCaptchRef = useRef()
 
     async function loginSubmit(){
         
-        const dataFromInputs = JSON.stringify({ nickanmeOrLogin, password })
-
+        const captchaToken = await reCaptchRef.current.getValue()
+        
+        const dataFromInputs = JSON.stringify({ nickanmeOrLogin, password, captchaToken })
+        
         try {
             const responce = await request("../../api/auth/login", "POST", dataFromInputs)
-            login(responce.nickname, responce.userId, responce.role)
+            login(responce.nickname, responce.userId, responce.role, captchaToken)
             router.push("/")
             dispatch(showAlert("", "warning"))//очищаем состояние alert в redux
+            await reCaptchRef.current.reset()
         } catch(e) {
             console.log(e)
             dispatch(showAlert(e.message, "warning"))
+            await reCaptchRef.current.reset()
         }
     }
 
@@ -48,6 +54,12 @@ export default function LogInPage () {
                                 <TextField onChange={(e) => setNickanmeOrLogin(e.target.value)} value={nickanmeOrLogin} name="email_or_login" className={styles.textInput} color={"secondary"} id="standard-password-input" label="Email or login" type="text" autoComplete="Off"/>
                                 <TextField onChange={(e) => setPassword(e.target.value)} value={password} name="password" className={styles.textInput} color={"secondary"} id="standard-password-input" label="Password" type="password" autoComplete="Off"/>
                             </div>
+                            <ReCAPTCHA 
+                                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
+                                theme="light"
+                                ref={reCaptchRef}
+                                size="normal"
+                            />
                             <div className={styles.window__content__buttons}>
                                 <Button disabled={loading} onClick={loginSubmit} variant="contained" size="medium" className={styles.formButton} >Log in</Button>
                             </div>
